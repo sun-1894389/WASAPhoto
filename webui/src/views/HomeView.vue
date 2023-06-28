65a8b414b2ebb645dd8615c370e301bce9d006b7
sun-1894389 <62235475+sun-1894389@users.noreply.github.com>
2023-06-28 14:31:17 +0200

    Add files via upload
    finished frontend

--- webui/src/views/HomeView.vue
+++ webui/src/views/HomeView.vue
@@ -1,98 +1,58 @@
 <script>
 export default {
-	data: function() {
+	data: function () {
 		return {
 			errormsg: null,
-			loading: false,
-			fountains: [],
+			photos: [],
 		}
 	},
-	methods: {
-		load() {
-			return load
-		},
-		async refresh() {
-			this.loading = true;
-			this.errormsg = null;
-			try {
-				let response = await this.$axios.get("/fountains/");
-				this.fountains = response.data;
-			} catch (e) {
-				this.errormsg = e.toString();
-			}
-			this.loading = false;
-		},
-		async newItem() {
-			this.$router.push("/new");
-		},
-		async deleteFountain(id) {
-			this.loading = true;
-			this.errormsg = null;
-			try {
-				await this.$axios.delete("/fountains/" + id);
 
-				await this.refresh();
+	methods: {
+		
+		async loadStream() {
+			try {
+				this.errormsg = null
+				// Home get: "/users/:id/home"
+				let response = await this.$axios.get("/users/" + localStorage.getItem('token') + "/home")
+
+				if (response.data != null){
+					this.photos = response.data
+				}
+				
 			} catch (e) {
-				this.errormsg = e.toString();
+				this.errormsg = e.toString()
 			}
-			this.loading = false;
 		}
 	},
-	mounted() {
-		this.refresh()
+
+	async mounted() {
+		await this.loadStream()
 	}
+
 }
 </script>
 
 <template>
-	<div>
-		<div
-			class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-			<h1 class="h2">Fountains list</h1>
-			<div class="btn-toolbar mb-2 mb-md-0">
-				<div class="btn-group me-2">
-					<button type="button" class="btn btn-sm btn-outline-secondary" @click="refresh">
-						Refresh
-					</button>
-				</div>
-				<div class="btn-group me-2">
-					<button type="button" class="btn btn-sm btn-outline-primary" @click="newItem">
-						New
-					</button>
-				</div>
-			</div>
-		</div>
-
+	<div class="container-fluid">
 		<ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
 
-		<LoadingSpinner v-if="loading"></LoadingSpinner>
-
-		<div class="card" v-if="fountains.length === 0">
-			<div class="card-body">
-				<p>No fountains in the database.</p>
-
-				<a href="javascript:" class="btn btn-primary" @click="newItem">Create a new fountain</a>
-			</div>
+		<div class="row">
+			<Photo
+				v-for="(photo,index) in photos"
+				:key="index"
+				:owner="photo.owner"
+				:photo_id="photo.photo_id"
+				:comments="photo.comments != nil ? photo.comments : []"
+				:likes="photo.likes != nil ? photo.likes : []"
+				:upload_date="photo.date"
+			/>
 		</div>
 
-		<div class="card" v-if="!loading" v-for="f in fountains">
-			<div class="card-header">
-				Fountain
-			</div>
-			<div class="card-body">
-				<p class="card-text">
-					Latitude: {{ f.latitude }}<br />
-					Longitude: {{ f.longitude }}<br />
-					Status: {{ f.status }}
-				</p>
-				<a href="javascript:" class="btn btn-danger" @click="deleteFountain(f.id)">Delete</a>
-			</div>
+		<div v-if="photos.length === 0" class="row ">
+			<h1 class="d-flex justify-content-center mt-5" style="color: white;">There's no content yet, follow somebody!</h1>
 		</div>
 	</div>
 </template>
 
-<style scoped>
-.card {
-	margin-bottom: 20px;
-}
+<style>
 </style>
